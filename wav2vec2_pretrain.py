8e1756e04b67a84f723881cd95fa5d59462c2468
jp1924 <jp42maru@gmail.com>
2024-04-05 01:00:14 +0000

    Add: load_dataset & preprocessor

--- wav2vec2_pretrain.py
+++ wav2vec2_pretrain.py
@@ -19,7 +19,6 @@ import os
 from typing import Any, Dict, List, Union
 
 import torch
-from accelerate.logging import get_logger
 from data import DataCollatorForWav2Vec2Pretraining
 from datasets import Dataset, concatenate_datasets, load_dataset
 from setproctitle import setproctitle
@@ -31,8 +30,9 @@ from transformers import (
     Wav2Vec2ForPreTraining,
     Wav2Vec2Processor,
     is_wandb_available,
-    set_seed,
 )
+from transformers import logging as hf_logging
+from transformers import set_seed
 from utils import (
     Wav2Vec2PretrainingArguments,
     default_sentence_norm,
@@ -41,13 +41,16 @@ from utils import (
 )
 from wav2vec2_pretrainer import Wav2Vec2Pretrainer
 
-logger = get_logger(__name__)
+hf_logging.set_verbosity_info()
+logger = hf_logging.get_logger("transformers")
 
 
 def main(train_args: Wav2Vec2PretrainingArguments):
-    def preprocessor(audio_ls, sentence_ls) -> Dict[str, List[Any]]:
+    def preprocessor(example: Dict[str, Union[List[Any], List[List[Any]]]]) -> Dict[str, List[Any]]:
+        sentence_ls = example["sentence"]
         sentence_ls = sentence_ls if isinstance(sentence_ls, list) else [sentence_ls]
 
+        audio_ls = example["audio"]
         audio_ls = audio_ls if isinstance(audio_ls, list) else [audio_ls]
         audio_ls = [audio["array"] for audio in audio_ls]
 
@@ -94,8 +97,9 @@ def main(train_args: Wav2Vec2PretrainingArguments):
             check_key: str = lambda key: (prefix in key)
             filter_data = [data_dict.pop(key) for key in list(data_dict.keys()) if check_key(key)]
             data_ls.extend(filter_data)
-        return data_ls
+        return concatenate_datasets(data_ls[0])
 
+    # load model, feature_extractor, tokenizer
     config = Wav2Vec2Config.from_pretrained(train_args.model_name_or_path)
     model = Wav2Vec2ForPreTraining(config)
 
@@ -107,35 +111,68 @@ def main(train_args: Wav2Vec2PretrainingArguments):
     if train_args.gradient_checkpointing:
         model.gradient_checkpointing_enable()
 
+    # load dataset & preprocess
     data_dict = dict()
     for dataset_name in train_args.dataset_names:
+        logger.info(f"load-{dataset_name}")
         dataset = load_dataset(dataset_name)
+
+        # DatasetDict이라서 이런식으로 해줘야 함.
+        column_names = set(sum(dataset.column_names.values(), []))
         with train_args.main_process_first(desc="data preprocess"):
 
+            cache_file_name = None
             if train_args.cache_file_name:
-                train_args.cache_file_name = {
-                    x: os.path.join(train_args.cache_dir, f"{x}_{train_args.cache_file_name}") for x in dataset
-                }
+                get_cache_path: str = lambda x: os.path.join(
+                    train_args.cache_dir, f"{name}-{x}_{train_args.cache_file_name}"
+                )
+                name = dataset_name.split("/")[-1]
+                cache_file_name = {x: get_cache_path(x) for x in dataset}
+
             dataset = dataset.map(
                 preprocessor,
                 num_proc=train_args.preprocessing_num_workers,
-                input_columns=[train_args.audio_column_name, train_args.sentence_column_name],
                 load_from_cache_file=True,
                 batched=train_args.preprocessing_batched,
-                cache_file_names=train_args.cache_file_name,
+                cache_file_names=cache_file_name,
                 batch_size=train_args.preprocessing_batch_size,
-                desc=dataset_name,
+                remove_columns=column_names,
+                desc=f"preprocess-{dataset_name}",
             )
         for data_key in dataset:
             if data_key not in data_dict:
                 data_dict[data_key] = []
 
-            data_dict[data_key].append(dataset)
+            specific_dataset = dataset[data_key]
 
-    train_dataset = collect_dataset(train_args.train_dataset_prefix)
-    valid_dataset = collect_dataset(train_args.valid_dataset_prefix)
-    test_dataset = collect_dataset(train_args.test_dataset_prefix)
+            added_data = [f"{dataset_name}-{data_key}"] * len(specific_dataset)
+            specific_dataset = specific_dataset.add_column("dataset_name", added_data)
 
+            data_dict[data_key].append(specific_dataset)
+
+    train_dataset = None
+    if train_args.do_train:
+        train_dataset = collect_dataset(train_args.train_dataset_prefix)
+        logger.info("train_dataset")
+        logger.info(train_dataset)
+
+    valid_dataset = None
+    if train_args.do_eval:
+        valid_dataset = collect_dataset(train_args.valid_dataset_prefix)
+        logger.info("valid_dataset")
+        logger.info(valid_dataset)
+
+    test_dataset = None
+    if train_args.do_predict:
+        test_dataset = collect_dataset(train_args.test_dataset_prefix)
+        logger.info("test_dataset")
+        logger.info(test_dataset)
+
+    # 6898663 >> 3.3% 정도 필터링 됨. 여기엔 tokenizing할 수 없는 문자, 음성 길이가 맞지 않는 문자 등 여러 요인으로 인해 필터링 된 데이터가 포함
+    # 7136987
+    # 238324
+
+    # set collator
     collator = DataCollatorForWav2Vec2Pretraining(
         model=model,
         feature_extractor=feature_extractor,
@@ -143,7 +180,14 @@ def main(train_args: Wav2Vec2PretrainingArguments):
         mask_time_prob=train_args.mask_time_prob,
         mask_time_length=train_args.mask_time_length,
     )
+    if train_args.torch_compile:
+        model = torch.compile(
+            model,
+            backend=train_args.torch_compile_backend,
+            mode=train_args.torch_compile_mode,
+        )
 
+    # set trainer
     trainer = Wav2Vec2Pretrainer(
         model=model,
         data_collator=collator,
@@ -161,9 +205,10 @@ def main(train_args: Wav2Vec2PretrainingArguments):
 
 
 def train(trainer: Wav2Vec2Pretrainer) -> None:
-    trainer.train(resume_from_checkpoint=trainer.args.resume_from_checkpoint)
+    train_args = trainer.args
+    trainer.train(resume_from_checkpoint=train_args.resume_from_checkpoint)
 
-    save_dir = os.path.join(trainer.args.output_dir, "last_model")
+    save_dir = os.path.join(train_args.output_dir, "last_model")
     trainer.save_model(save_dir)
     trainer.save_metrics()
     trainer.save_state()
