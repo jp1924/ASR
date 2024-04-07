ec27255b733e38da43f16221814025426dc67c00
jp1924 <jp42maru@gmail.com>
2024-04-07 07:24:04 +0000

    Update: script

--- script/run_wav2vec2_pretrain_ddp.sh
+++ script/run_wav2vec2_pretrain_ddp.sh
@@ -1,8 +1,15 @@
-torchrun --nproc_per_node=4 /root/workspace/wav2vec2_pretrain.py \
+export TORCH_DISTRIBUTED_DEBUG="DETAIL"
+export TORCHDYNAMO_DISABLE="1"
+
+torchrun --nproc_per_node=4 \
+    /root/workspace/wav2vec2_pretrain.py \
     --output_dir=/root/output_dir \
-    --run_name=wav2vec2-pretrain \
+    --run_name=wav2vec2-pretrain-debug \
     --model_name_or_path=/root/model \
     --preprocessing_num_workers=10 \
+    --per_device_train_batch_size=6 \
+    --gradient_accumulation_steps=1 \
+    --per_device_eval_batch_size=2 \
     --overwrite_cache=false \
     --cache_dir=false \
     --seed=42 \
@@ -11,24 +18,21 @@ torchrun --nproc_per_node=4 /root/workspace/wav2vec2_pretrain.py \
     --do_predict=true \
     --report_to=none \
     --learning_rate=5e-4 \
-    --warmup_ratio=0.08 \
+    --warmup_ratio=0.3 \
     --weight_decay=0.01 \
     --evaluation_strategy=no \
     --save_strategy=no \
     --logging_strategy=steps \
+    --logging_steps=1 \
     --fp16=true \
     --dataset_names \
         jp1924/KsponSpeech \
-        jp1924/KrespSpeech \
-        jp1924/KoreaSpeech \
-        jp1924/KconfSpeech \
-    --train_dataset_prefix \
-        train \
-    --valid_dataset_prefix \
-        validation \
-        dev \
-    --test_dataset_prefix \
-        eval_clean \
-        eval_other \
+    --train_dataset_prefix=train \
+    --valid_dataset_prefix validation dev \
+    --test_dataset_prefix eval_clean eval_other \
     --cache_file_name=preprocessor.arrow \
-    --cache_dir=/root/.preprocessor_cache_dir
+    --cache_dir=/root/.preprocessor_cache_dir \
+    --gradient_checkpointing=true \
+    --remove_unused_columns=false \
+    --group_by_length=false \
+    --torch_compile=true
