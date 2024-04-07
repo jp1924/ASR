c00be72d70e64c41dfe1156c412b732f0682b4b7
jp1924 <jp42maru@gmail.com>
2024-04-07 16:15:17 +0000

    Add: _supports_flash_attn_2

--- models/wav2vec2/modeling_wav2vec2.py
+++ models/wav2vec2/modeling_wav2vec2.py
@@ -1420,6 +1420,7 @@ class Wav2Vec2PreTrainedModel(PreTrainedModel):
     base_model_prefix = "wav2vec2"
     main_input_name = "input_values"
     supports_gradient_checkpointing = True
+    _supports_flash_attn_2 = True
 
     def _init_weights(self, module):
         """Initialize the weights"""
