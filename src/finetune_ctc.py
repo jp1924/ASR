c0b64ebf50f84d593455e5517562af28911b6079
jp1924 <jp42maru@gmail.com>
2025-01-27 01:34:24 +0900

    Add: args

--- src/finetune_ctc.py
+++ src/finetune_ctc.py
@@ -1,3 +1,4 @@
+import json
 import os
 import time
 import unicodedata
@@ -8,7 +9,7 @@ from typing import Callable, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
-from data_processor import wav2vec2_ctc_finetune_preprocessor
+from data_processor import wav2vec2_finetune_ctc_preprocessor
 from datasets import Dataset, concatenate_datasets, load_dataset
 from evaluate import load
 from setproctitle import setproctitle
@@ -31,7 +32,212 @@ hf_logging.set_verbosity_info()
 logger = hf_logging.get_logger("transformers")
 
 
-def main(train_args: Wav2Vec2FinetuningArguments) -> None:
+PROCESS_FUNC_MAP = {
+    "wav2vec2_finetune_ctc": wav2vec2_finetune_ctc_preprocessor,
+}
+
+
+@dataclass
+class DataPipelineArguments:
+    # data
+    dataset_repo_ls: List[str] = field(
+        default_factory=list,
+        metadata={"help": "The name of the dataset to use (via the datasets library)."},
+    )
+    preprocessing_num_workers: int = field(
+        default=4,
+        metadata={"help": "The number of processes to use for the preprocessing."},
+    )
+    preprocessing_batch_size: int = field(
+        default=1000,
+        metadata={"help": "The number of processes to use for the preprocessing."},
+    )
+    audio_column_name: str = field(
+        default="audio",
+        metadata={"help": "Column in the dataset that contains speech file path. Defaults to 'audio'"},
+    )
+
+    audio_min_seq: int = field(
+        default=1,
+        metadata={"help": "Filter out audio files that are longer than `audio_min_seq` seconds"},
+    )
+    audio_max_seq: int = field(
+        default=512,
+        metadata={"help": "Filter out audio files that are shorter than `audio_max_seq` seconds"},
+    )
+    train_dataset_prefix: List[str] = field(
+        default_factory=list,
+        metadata={"help": "A prefix required to distinguish splits in the data loaded by load_dataset."},
+    )
+    valid_dataset_prefix: List[str] = field(
+        default_factory=list,
+        metadata={"help": "A prefix required to distinguish splits in the data loaded by load_dataset."},
+    )
+    test_dataset_prefix: List[str] = field(
+        default_factory=list,
+        metadata={"help": "A prefix required to distinguish splits in the data loaded by load_dataset."},
+    )
+    data_truncate_map: Optional[Union[dict, str]] = field(
+        default="{}",
+        metadata={"help": "A map to truncate part of the data. {'repo_name': {'train': 3000, 'validation': 1500}}."},
+    )
+    data_name_map: Optional[Union[dict, str]] = field(
+        default="{}",
+        metadata={"help": "A map to config_name of the data. {'repo_name': 'data_config_name'"},
+    )
+    do_data_main_process_first: bool = field(
+        default=False,
+        metadata={"help": "Whether to process the main data first."},
+    )
+    cache_dir: str = field(
+        default="",
+        metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
+    )
+    data_preprocessor_type: str = field(
+        default="wav2vec2-pretrain",
+        metadata={"help": "Data preprocessor type."},
+    )
+
+
+@dataclass
+class TrainPipelineArguments:
+    # model
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models."}
+    )
+
+    pad_to_multiple_of: Optional[int] = field(
+        default=0,
+        metadata={
+            "help": "If set will pad the sequence to a multiple of the provided value. This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >= 7.5 (Volta)."
+        },
+    )
+
+    attn_implementation: str = field(
+        default="eager",
+        metadata={
+            "help": "Length of each vector mask span to mask along the time axis in the contrastive task. If omitted, will pull value from model config."
+        },
+    )
+    sampling_rate: int = field(
+        default=16000,
+        metadata={"help": ""},
+    )
+
+    profiling: bool = field(
+        default=False,
+        metadata={"help": "Enable profiling."},
+    )
+    profile_kwargs: Optional[Union[dict, str]] = field(
+        default="{}",
+        metadata={"help": ""},
+    )
+    config_kwargs: Optional[Union[dict, str]] = field(
+        default="{}",
+        metadata={"help": ""},
+    )
+    model_kwargs: Optional[Union[dict, str]] = field(
+        default="{}",
+        metadata={"help": ""},
+    )
+    processor_kwargs: Optional[Union[dict, str]] = field(
+        default="{}",
+        metadata={"help": ""},
+    )
+
+
+@dataclass
+class FinetuneCTCArguments(TrainingArguments, DataPipelineArguments, TrainPipelineArguments):
+    output_dir: str = field(
+        default=None,
+        metadata={"help": "The output directory where the model predictions and checkpoints will be written."},
+    )
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        def _convert_str_dict(passed_value: dict):
+            "Safely checks that a passed value is a dictionary and converts any string values to their appropriate types."
+            for key, value in passed_value.items():
+                if isinstance(value, dict):
+                    passed_value[key] = _convert_str_dict(value)
+                elif isinstance(value, str):
+                    # First check for bool and convert
+                    if value.lower() in ("true", "false"):
+                        passed_value[key] = value.lower() == "true"
+                    # Check for digit
+                    elif value.isdigit():
+                        passed_value[key] = int(value)
+                    elif value.replace(".", "", 1).isdigit():
+                        passed_value[key] = float(value)
+
+            return passed_value
+
+        _ADDITIONAL_VALID_DICT_FILEDS = [
+            "data_truncate_map",
+            "data_name_map",
+            "config_kwargs",
+            "model_kwargs",
+            "processor_kwargs",
+            "profile_kwargs",
+        ]
+        _VALID_LIST_FIELDS = [
+            "dataset_repo_ls",
+            "train_dataset_prefix",
+            "valid_dataset_prefix",
+            "test_dataset_prefix",
+        ]
+
+        # copied from: transformers/training_args.py/__post_init__()
+        for field in _ADDITIONAL_VALID_DICT_FILEDS:
+            passed_value = getattr(self, field)
+            # We only want to do this if the str starts with a bracket to indiciate a `dict`
+            # else its likely a filename if supported
+            if isinstance(passed_value, str) and passed_value.startswith("{"):
+                loaded_dict = json.loads(passed_value)
+                # Convert str values to types if applicable
+                loaded_dict = _convert_str_dict(loaded_dict)
+                setattr(self, field, loaded_dict)
+            elif isinstance(passed_value, dict) or passed_value is None:
+                pass
+            else:
+                raise ValueError(f"{field}은 dict로 설정해야 함.")
+
+        for field in _VALID_LIST_FIELDS:
+            passed_value = getattr(self, field)
+            if isinstance(passed_value, str) and passed_value.startswith("["):
+                loaded_list = json.loads(passed_value)
+                setattr(self, field, loaded_list)
+            elif isinstance(passed_value, list) or passed_value is None:
+                pass
+            else:
+                raise ValueError(f"{field}은 list로 설정해야 함.")
+
+        self.config_kwargs = {
+            **self.config_kwargs,
+            "attn_implementation": self.attn_implementation,
+        }
+
+        self.cache_dir = Path(self.cache_dir) if self.cache_dir else None
+        self.model_name_or_path = self.resume_from_checkpoint or self.model_name_or_path
+
+    @property
+    def is_local_process_zero(self) -> bool:
+        return self.local_process_index == 0
+
+    @property
+    def is_world_process_zero(self) -> bool:
+        from transformers.utils import is_sagemaker_mp_enabled
+
+        if is_sagemaker_mp_enabled():
+            import smdistributed.modelparallel.torch as smp  # type: ignore
+
+            return smp.rank() == 0
+        else:
+            return self.process_index == 0
+
+
+def main(train_args: FinetuneCTCArguments) -> None:
     def processing_datasets(func: Callable) -> Tuple[Optional[Dataset], Optional[Dataset], Optional[Dataset]]:
         def process_dataset(
             dataset: Dataset,
@@ -168,17 +374,15 @@ def main(train_args: Wav2Vec2FinetuningArguments) -> None:
             fullgraph=True,
         )
 
-    match train_args.data_preprocessor_type:
-        case "wav2vec2_ctc_finetune":
-            preprocessor_func = wav2vec2_ctc_finetune_preprocessor
-
     with (
         train_args.main_process_first(desc="main_process_first")
         if train_args.do_data_main_process_first
         else nullcontext()
     ):
         # load datasets
-        train_dataset, valid_dataset, test_dataset = processing_datasets(preprocessor_func)
+        train_dataset, valid_dataset, test_dataset = processing_datasets(
+            PROCESS_FUNC_MAP[train_args.data_preprocessor_type]
+        )
 
     collator = DataCollatorCTCWithPadding(
         processor=processor,
@@ -208,7 +412,7 @@ def main(train_args: Wav2Vec2FinetuningArguments) -> None:
         logger.info("do_predict 코드는 아직 작성 중")
 
 
-def train(trainer: Trainer, train_args: Wav2Vec2FinetuningArguments) -> None:
+def train(trainer: Trainer, train_args: FinetuneCTCArguments) -> None:
     trainer.train(resume_from_checkpoint=train_args.resume_from_checkpoint)
 
     save_dir = os.path.join(train_args.output_dir, "last_model")
@@ -223,7 +427,7 @@ def valid(trainer: Trainer, valid_datasets: Optional[Union[Dataset, Dict[str, Da
 
 
 if __name__ == "__main__":
-    parser = HfArgumentParser([Wav2Vec2FinetuningArguments])
+    parser = HfArgumentParser([FinetuneCTCArguments])
     train_args, remain_args = parser.parse_args_into_dataclasses(return_remaining_strings=True)
 
     if remain_args and is_main_process(train_args.local_rank):
